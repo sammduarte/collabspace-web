2cbc6a260be0652d7873c263ffe2b29c2d95a466
Samuel Duarte <samueljaba2010@hotmail.com>
2023-09-14 21:52:29 -0300

    Finish reactions post an comments

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,7 +1,12 @@
+import { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-
-import { Trash } from "phosphor-react";
 import moment from "moment";
+
+import { Trash, ThumbsUp } from "phosphor-react";
+
+import { createReaction, deleteReaction } from "../../services/reactions";
+
+import { IReaction } from "../../services/reactions/types";
 
 import Avatar from "../AvatarSquare";
 
@@ -9,7 +14,15 @@ import { DiffToString } from "../../utils/date";
 
 import { useAuthentication } from "../../contexts/Authentication";
 
-import { Container, CommentBox, AuthorAndTime, ButtonDelete } from "./styles";
+import {
+  Container,
+  CommentBox,
+  InputArea,
+  AuthorAndTime,
+  Interactions,
+  ButtonDelete,
+} from "./styles";
+import { toast } from "react-toastify";
 
 interface CommentProps {
   postAuthorId: string;
@@ -18,7 +31,7 @@ interface CommentProps {
   authorName: string;
   commentId: string;
   content: string;
-  reactions: any[];
+  reactions: IReaction[];
   commentedAt: string;
   onDelete(id: string): void;
 }
@@ -37,6 +50,69 @@ const Comment: React.FC<CommentProps> = ({
   const navigate = useNavigate();
   const { user } = useAuthentication();
 
+  const [commentReactions, setCommentReactions] = useState(reactions);
+
+  const [userReacted, setUserReacted] = useState(
+    commentReactions.some((reaction) => reaction.user.id === user?.id),
+  );
+
+  const handleCreateReaction = useCallback(async () => {
+    try {
+      const { result, data } = await createReaction({
+        commentId,
+        entityType: 1,
+      });
+
+      if (result === "success") {
+        if (data) {
+          setCommentReactions((prevState) => {
+            const commentReactions = [...prevState];
+
+            commentReactions.unshift(data);
+
+            return commentReactions;
+          });
+
+          setUserReacted(true);
+        }
+      }
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  }, [commentId]);
+
+  const handleDeleteReaction = useCallback(async (reactionId: string) => {
+    try {
+      const { result } = await deleteReaction({
+        reactionId,
+      });
+
+      if (result === "success") {
+        setCommentReactions((prevState) =>
+          prevState.filter((reaction) => reaction.id !== reactionId),
+        );
+
+        setUserReacted(false);
+      }
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  }, []);
+
+  function toggleReaction() {
+    if (userReacted) {
+      const reaction = commentReactions.find(
+        (reaction) => reaction.user.id === user?.id,
+      );
+
+      if (reaction) handleDeleteReaction(reaction.id);
+
+      return;
+    }
+
+    handleCreateReaction();
+  }
+
   function handleMe() {
     navigate(`/me/${authorId}`);
   }
@@ -49,20 +125,32 @@ const Comment: React.FC<CommentProps> = ({
       />
 
       <CommentBox>
-        <AuthorAndTime>
-          <h1 onClick={handleMe}>{authorName}</h1>
-          <time>
-            Cerca de {DiffToString(moment().diff(commentedAt, "seconds"))}
-          </time>
+        <InputArea>
+          <AuthorAndTime>
+            <h1 onClick={handleMe}>{authorName}</h1>
+            <time>
+              Cerca de {DiffToString(moment().diff(commentedAt, "seconds"))}
+            </time>
 
-          {(user && user.id === authorId) || user?.id === postAuthorId ? (
-            <ButtonDelete onClick={() => onDelete(commentId)}>
-              <Trash size={22} />
-            </ButtonDelete>
-          ) : null}
-        </AuthorAndTime>
+            {(user && user.id === authorId) || user?.id === postAuthorId ? (
+              <ButtonDelete onClick={() => onDelete(commentId)}>
+                <Trash size={22} />
+              </ButtonDelete>
+            ) : null}
+          </AuthorAndTime>
 
-        <p>{content}</p>
+          <p>{content}</p>
+        </InputArea>
+
+        <Interactions>
+          <ThumbsUp
+            onClick={toggleReaction}
+            size={18}
+            weight={userReacted ? "fill" : "regular"}
+          />
+          <span>•</span>
+          <span>{commentReactions.length}</span>
+        </Interactions>
       </CommentBox>
     </Container>
   );
--- src/components/Comment/styles.ts
+++ src/components/Comment/styles.ts
@@ -7,6 +7,12 @@ export const Container = styled.div`
 
 export const CommentBox = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const InputArea = styled.div`
   background: var(--zinc-900);
   border-radius: 8px;
   padding: 1rem 1rem 1.5rem 1rem;
@@ -37,9 +43,32 @@ export const AuthorAndTime = styled.div`
   }
 
   time {
-    max-width: max-content;
     font-size: 0.75rem;
     color: var(--zinc-500);
+  }
+`;
+
+export const Interactions = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 0 8px;
+
+  font-size: 0.875rem;
+  color: var(--zinc-400);
+
+  svg {
+    margin-bottom: 2px;
+    color: var(--emerald-600);
+    cursor: pointer;
+  }
+
+  span {
+    cursor: pointer;
+
+    &:hover {
+      text-decoration: underline;
+    }
   }
 `;
 
