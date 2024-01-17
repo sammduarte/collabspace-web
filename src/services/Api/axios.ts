332ee13a19d533992337785a401a6c001230f34e
Samuel Duarte <samueljaba2010@hotmail.com>
2024-01-16 23:54:40 -0300

    Added env api url

--- src/services/Api/axios.ts
+++ src/services/Api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getAPIClient = () => {
   const api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
       Authorization: `Bearer ${JSON.parse(
         localStorage.getItem("token") as string,
